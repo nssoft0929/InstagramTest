d8fb40a08d164f540442a2f934ff13c91550ed1f
nssoft <namsig0929@naver.com>
2019-11-14 15:14:29 +0900

    Toolbar + icon / CameraActivity 생성 만

--- app/src/main/java/com/nssoft/instagramtest/MainActivity.java
+++ app/src/main/java/com/nssoft/instagramtest/MainActivity.java
@@ -1,12 +1,15 @@
 package com.nssoft.instagramtest;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.ActionMenuItem;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -22,12 +25,22 @@ public class MainActivity extends AppCompatActivity {
     Frag_activity frag_activity;
     Frag_account frag_account;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         bnv=findViewById(R.id.bnv);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_camera_alt_black_24dp);
+
+
         frag_home=new Frag_home();
         frag_search=new Frag_search();
         frag_add=new Frag_add();
@@ -63,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-    }
+    }//onCreate
 
     void setFrag(int n){
         fm=getSupportFragmentManager();
@@ -72,27 +85,52 @@ public class MainActivity extends AppCompatActivity {
         switch (n){
             case 0:
                 ft.replace(R.id.frame, frag_home);
+                ft.addToBackStack(null);
                 ft.commit();
                 break;
             case 1:
                 ft.replace(R.id.frame, frag_search);
+                ft.addToBackStack(null);
                 ft.commit();
                 break;
                 case 2:
                 ft.replace(R.id.frame, frag_add);
+                ft.addToBackStack(null);
                 ft.commit();
                 break;
                 case 3:
                 ft.replace(R.id.frame, frag_activity);
+                ft.addToBackStack(null);
                 ft.commit();
                 break;
                 case 4:
                 ft.replace(R.id.frame, frag_account);
+                ft.addToBackStack(null);
                 ft.commit();
                 break;
 
         }
     }//setFrag
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                //TODO CameraActivity
+                break;
+            }
+            case R.id.send:
+                //TODO DirectActivity
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
 }
